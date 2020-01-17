385d7342e3092450f6b2316e19b59c9da8947af5
simon <hanzhenfeng@wuxicloud.com>
2020-01-17 16:01:27 +0800

    第一个重构

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,35 +1,38 @@
 public class Length {
+    public static final String UNIT_F = "f";
+    public static final String UNIT_YARD = "yard";
+    public static final String UNIT_INCH = "inch";
     private final double value;
     private final String unit;
 
-    public Length(double val, String uinnt) {
+    public Length(double val, String unit) {
         this.value = val;
-        this.unit = uinnt;
+        this.unit = unit;
     }
 
-    public Length as(String u) {
+    public Length as(String unit) {
         Length len = this;
-        if (this.unit.equals("f")) {
-            if (u.equals("yard")) {
-                len = new Length(this.value / 3, u);
-            } else if (u.equals("inch")) {
-                len = new Length(this.value * 12, u);
+        if (this.unit.equals(UNIT_F)) {
+            if (unit.equals(UNIT_YARD)) {
+                len = new Length(this.value / 3, unit);
+            } else if (unit.equals(UNIT_INCH)) {
+                len = new Length(this.value * 12, unit);
             }
         }
 
-        if (this.unit.equals("yard")) {
-            if (u.equals("inch")) {
-                len = new Length(this.value * 36, u);
-            } else if (u.equals("f")){
-                len = new Length(this.value * 3, u);
+        if (this.unit.equals(UNIT_YARD)) {
+            if (unit.equals(UNIT_INCH)) {
+                len = new Length(this.value * 36, unit);
+            } else if (unit.equals(UNIT_F)){
+                len = new Length(this.value * 3, unit);
             }
         }
 
-        if (this.unit.equals("inch")) {
-            if (u.equals("f")) {
-                len = new Length(this.value / 12, u);
-            } else if (u.equals("yard")) {
-                len = new Length(this.value / 36, u);
+        if (this.unit.equals(UNIT_INCH)) {
+            if (unit.equals(UNIT_F)) {
+                len = new Length(this.value / 12, unit);
+            } else if (unit.equals(UNIT_YARD)) {
+                len = new Length(this.value / 36, unit);
             }
         }
 
